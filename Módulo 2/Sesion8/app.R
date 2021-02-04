17bb5db4ea2e04193408106566fe20ff70394e64
MarkHG <markhg@ciencias.unam.mx>
2021-02-04 06:53:01 +0100

    first commit

--- Módulo 2/Sesion8/app.R
+++ Módulo 2/Sesion8/app.R
@@ -1,13 +1,22 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-
-
-shinyUI(
+d1011 <- read.csv("match.data.csv")
+# Define UI for application that draws a histogram
+ui <- shinyUI(
     pageWithSidebar(
         headerPanel("Aplicacion basica con Shiny"),
         sidebarPanel(
             p("Crear plots con el DF 'auto'"), 
             selectInput("x", "Seleccione el valor de X",
-                        choices = names(mtcars))
+                        choices = names(d1011))
         ),
         
         mainPanel(
@@ -22,8 +31,8 @@ shinyUI(
                                       tabPanel("imágenes",  #Pestaña de imágenes <-----------
                                                img( src = "1.png", 
                                                     height = 450, width = 450),
-                                              
-                                                img( src = "2.png", 
+                                               
+                                               img( src = "2.png", 
                                                     height = 450, width = 450)
                                       ), 
                                       
@@ -36,3 +45,32 @@ shinyUI(
     )
     
 )
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$output_text <- renderText(paste("mpg~", input$x))   #Titulo del main Panel
+    
+    #Gráficas                       <----------
+    output$output_plot <- renderPlot( plot( as.formula(paste("mpg ~", input$x)),
+                                            data = d1011) )
+    
+    #imprimiendo el summary       <----------                                  
+    output$summary <- renderPrint({
+        summary(d1011)
+    })
+    
+    # Agregando el dataframe       <----------
+    output$table <- renderTable({ 
+        data.frame(d1011)
+    })
+    
+    #Agregando el data table       <----------
+    output$data_table <- renderDataTable({d1011}, 
+                                         options = list(aLengthMenu = c(5,25,50),
+                                                        iDisplayLength = 5))
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
